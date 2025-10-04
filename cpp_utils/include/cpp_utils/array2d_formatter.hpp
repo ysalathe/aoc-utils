82c490e4da43e115d61d289be907f4e34b15d47b
Yves Salathe <ysa@chatnet.ch>
2025-10-04 16:58:28 +0200

    Move custom formatter for array2d into separate namespace

--- cpp_utils/include/cpp_utils/array2d_formatter.hpp
+++ cpp_utils/include/cpp_utils/array2d_formatter.hpp
@@ -7,6 +7,26 @@
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
+
+namespace _array2d_formatter_detail {
+
+  // Common formatter for classes derived from Array2DBase<T>
+  template <typename T>
+  struct array2d_formatter {
+    template <typename FormatContext>
+    auto format(const T& array, FormatContext& ctx) const -> decltype(ctx.out()) {
+      auto result =
+          fmt::format_to(ctx.out(), "Array2DBase({}x{})\n", array.num_rows(), array.num_columns());
+      for (int row = 0; row < array.num_rows(); ++row) {
+        auto row_range = array.range_from({row, 0});
+        result = fmt::format_to(ctx.out(), "{}\n", fmt::join(row_range, " "));
+      }
+      return result;
+    }
+  };
+
+}
+
 namespace fmt {
 
   // Disable ranges formatter for Array2DBase and its derivatives
@@ -25,36 +45,19 @@ namespace fmt {
       : std::integral_constant<fmt::range_format, fmt::range_format::disabled>
   { };
 
-  // Common formatter for classes derived from Array2DBase<T>
+  // Custom formatter for Array2DBase<T> and its derivatives
   template <typename T>
-  struct array2d_formatter {
-    template <typename FormatContext>
-    auto format(const T& array, FormatContext& ctx) const -> decltype(ctx.out()) {
-      auto result =
-          fmt::format_to(ctx.out(), "Array2DBase({}x{})\n", array.num_rows(), array.num_columns());
-      for (int row = 0; row < array.num_rows(); ++row) {
-        auto row_range = array.range_from({row, 0});
-        result = fmt::format_to(ctx.out(), "{}\n", fmt::join(row_range, " "));
-      }
-      return result;
-    }
-  };
-
-  // Custom formatter for Array2DBase<T>
-  template <typename T>
-  struct formatter<cpp_utils::Array2DBase<T>> : array2d_formatter<cpp_utils::Array2DBase<T>> {
+  struct formatter<cpp_utils::Array2DBase<T>> : _array2d_formatter_detail::array2d_formatter<cpp_utils::Array2DBase<T>> {
     constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) { return ctx.begin(); }
   };
 
-  // Custom formatter for Array2D<T>
   template <typename T>
-  struct formatter<cpp_utils::Array2D<T>> : array2d_formatter<cpp_utils::Array2D<T>> {
+  struct formatter<cpp_utils::Array2D<T>> : _array2d_formatter_detail::array2d_formatter<cpp_utils::Array2D<T>> {
     constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) { return ctx.begin(); }
   };
 
-  // Custom formatter for SparseArray2D<T>
   template <typename T>
-  struct formatter<cpp_utils::SparseArray2D<T>> : array2d_formatter<cpp_utils::SparseArray2D<T>> {
+  struct formatter<cpp_utils::SparseArray2D<T>> : _array2d_formatter_detail::array2d_formatter<cpp_utils::SparseArray2D<T>> {
     constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) { return ctx.begin(); }
   };
 
