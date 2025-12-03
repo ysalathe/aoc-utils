2797a0b48c1e33cb902bd6dea144c6ff8af71010
Yves Salathé <yves.salathe@zhinst.com>
2025-12-03 11:21:10 +0100

    Improve utility function for integer division

--- cpp_utils/include/cpp_utils/math.hpp
+++ cpp_utils/include/cpp_utils/math.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <limits>
+
 namespace cpp_utils {
 
-  inline auto ceilDiv(auto numerator, auto denominator) {
+  template <typename T>
+    requires std::numeric_limits<T>::is_integer
+  inline auto ceilDiv(T numerator, T denominator) {
     // Computes the ceiling of the division numerator / denominator
-    // The arguments are assumed to be integers.
+    // The argument type must be integers.
     return (numerator + denominator - 1) / denominator;
   }
 
