a8fc41b8fac0ce737c5b10bf705ade85881eaf5c
Yves Salathe <ysa@chatnet.ch>
2026-01-12 23:09:10 +0100

    Add function for combining hashes

--- cpp_utils/include/cpp_utils/math.hpp
+++ cpp_utils/include/cpp_utils/math.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <limits>
 
 namespace cpp_utils {
@@ -10,6 +11,12 @@ namespace cpp_utils {
     // Computes the ceiling of the division numerator / denominator
     // The argument type must be integers.
     return (numerator + denominator - 1) / denominator;
+  }
+
+  template <class T>
+  inline void hashCombine(std::size_t& seed, const T& v) {
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
   }
 
 }  // namespace cpp_utils
